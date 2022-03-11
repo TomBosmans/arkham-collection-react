65f2668782abc48c7573e281077f7424fd45b980
Tom Bosmans <tom.94.bosmans@gmail.com>
2022-03-11 15:31:48 +0100

    Add linting config

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
+import React from "react"
+import ReactDOM from "react-dom"
+import { store } from "./app/store"
+import { Provider } from "react-redux"
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>hallo world</Provider>
   </React.StrictMode>,
   document.getElementById("root")
-);
+)
